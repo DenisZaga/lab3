37f393043c4f8365afb5ea6b4af51e24b56f58ad
denis.zagorulko <denys_zagorulko@knu.ua>
2023-05-25 16:34:05 +0300

    add alt

--- script/script.js
+++ script/script.js
@@ -1,48 +1,46 @@
-   
-   
-    document.getElementById('button').addEventListener('click', function () {
-            var massInput = document.getElementById('mass').value;
-            var valueInput = document.getElementById('value').value;
-            var array = massInput.split(',').map(function (num) {
-                return parseInt(num.trim(), 10);
-            });
-            var searchValue = parseInt(valueInput, 10);
+document.getElementById('button').addEventListener('click', function () {
+    let massInput = document.getElementById('mass').value;
+    let valueInput = document.getElementById('value').value;
+    let array = massInput.split(',').map(function (num) {
+        return parseInt(num.trim(), 10);
+    });
+    let searchValue = parseInt(valueInput, 10);
 
-            linearSearch(array, searchValue);
+    linearSearch(array, searchValue);
+});
+
+function linearSearch(array, searchValue) {
+    let output = document.getElementById('output');
+    output.innerHTML = '';
+    let foundIndexes = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let div = document.createElement('div');
+
+        array.forEach(function (num, index) {
+            let span = document.createElement('span');
+            span.textContent = num + ' ';
+
+            if (index === i) {
+                span.classList.add('active');
+            }
+
+            div.appendChild(span);
         });
 
-        function linearSearch(array, searchValue) {
-            var output = document.getElementById('output');
-            output.innerHTML = '';
-            var foundIndexes = [];
-
-            for (var i = 0; i < array.length; i++) {
-                var div = document.createElement('div');
-
-                array.forEach(function (num, index) {
-                    var span = document.createElement('span');
-                    span.textContent = num + ' ';
-
-                    if (index === i) {
-                        span.classList.add('active');
-                    }
-
-                    div.appendChild(span);
-                });
-
-                if (array[i] === searchValue) {
-                    foundIndexes.push(i + 1);
-                }
-
-                output.appendChild(div);
-            }
-
-            var result = document.createElement('div');
-            if (foundIndexes.length > 0) {
-                result.textContent = 'Значення ' + searchValue + ' знайдено під порядковим номером: ' + foundIndexes.join(', ') + '.';
-            } else {
-                result.textContent = 'Значення ' + searchValue + ' не знайдено у масиві';
-            }
-
-            output.appendChild(result);
+        if (array[i] === searchValue) {
+            foundIndexes.push(i + 1);
         }
+
+        output.appendChild(div);
+    }
+
+    let result = document.createElement('div');
+    if (foundIndexes.length > 0) {
+        result.textContent = 'Значення ' + searchValue + ' знайдено під порядковим номером: ' + foundIndexes.join(', ') + '.';
+    } else {
+        result.textContent = 'Значення ' + searchValue + ' не знайдено у масиві';
+    }
+
+    output.appendChild(result);
+}
